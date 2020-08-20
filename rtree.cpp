038f22f6f45ae537167ebdebce33a69a194817f1
jainvasu631 <jain.vasu631@gmail.com>
2020-08-20 19:21:15 +0530

    Added Scaffolding of Main While Loop

--- rtree.cpp
+++ rtree.cpp
@@ -1,4 +1,4 @@
-#include "rtree.h";
+#include "rtree.h"
 
 using namespace std;
 
@@ -13,7 +13,35 @@ int main(int argc, char const *argv[])
     DIMENSION = atoi(argv[3]); // Third Argument is Number of Dimension d
     string output_filename = argv[4]; // Fourth Argument is output filename
 
+    fstream query_file, output_file;
+    query_file.open(query_filename,ios::in); // Input File to fetch commands
+    query_file.open(output_filename,ios::out); // Output File to write output
+
+    string line;
     
-    
+    // Query Processing Loop
+    while (getline(query_file, line)){
+        istringstream iss(line);
+        string command_word; 
+        iss >> command_word;
+        Command command = string_to_command(command_word); // Enum Version of Command Word
+        
+        // Depending on what command is we process it
+        switch (command){
+            case Command::Bulkload:
+                /* code */
+                break;
+            case Command::Insert:
+                /* code */
+                break;
+            case Command::Query:
+                /* code */
+                break;
+            
+            default: // Error Case
+                break;
+        }
+    }    
+
     return EXIT_SUCCESS;
 }
